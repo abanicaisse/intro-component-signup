e14f5cb04b084f46a4e93b3516ee3d6bf741da5d
Aba Wandjovu Nicaisse <abawandjovunicaise@gmail.com>
2021-12-12 18:31:03 +0300

    Wrote the JS warning user if form isn't  filled correctly

--- main.js
+++ main.js
@@ -1,34 +1,54 @@
 const form = document.getElementById("form");
-const input = document.querySelectorAll("input");
+const inputs = document.querySelectorAll("input");
 
 form.addEventListener("submit", showError);
 //check if the correct password is enterred
 const checkPassWord = () => {
     const passWord = document.getElementById("password");
     const passRegex = /^(?=.*[A-Z])(?=.*[!@#$%&*])(?=.*[0-9])(?=.*[a-z]).{8,}$/;
-    if (!passRegex.test(passWord.value)) return passWord.parentElement.classList.add("error"); 
+    if (!passRegex.test(passWord.value)) {
+        passWord.parentElement.classList.add("error"); 
+        passWord.parentElement.nextElementSibling.style.display = "block";
+        return passRegex.test(passWord.value);
+    }
 }
 //Check if the correct email is enterred
 const checkEmail = () => {
     const email = document.getElementById("email");
     const emailRegex = /^\w+@\w+/gi 
-    if (!emailRegex.test(email.value)) return email.parentElement.classList.add("error");
+    if (!emailRegex.test(email.value)) {
+        email.parentElement.classList.add("error");
+        email.parentElement.nextElementSibling.style.display = "block";
+        return emailRegex.test(email.value);
+    }
 }
 //check if any text input is not empty
 const checkText = () => {
     const arr = Array.from(document.querySelectorAll("input[type='text'"));
-    arr.forEach((input) => input.value === "" ? input.parentElement.classList.add("error") : input.value);
+    arr.forEach((input) =>  {
+        if(input.value === ""){
+            input.parentElement.classList.add("error")
+            input.parentElement.nextElementSibling.style.display = "block";
+            return (input.value === "");
+        }
+    });
 }
-//Show erros on submit
+//Show errors on submit
 function showError(e) {
     e.preventDefault();
-    checkText();
-    checkEmail();
-    checkPassWord();
+    if (!checkText() || !checkEmail() || !checkPassWord()) {
+        checkText();
+        checkEmail();
+        checkPassWord();
+    }
 }
 //remove errors when typing in an input field
-Array.from(input).forEach(input => {
-    input.addEventListener("keyup", () => input.parentElement.classList.remove("error"))
+Array.from(inputs).forEach(input => {
+    input.addEventListener("keyup", () => {
+        input.parentElement.classList.remove("error")
+        // console.log(input)
+        input.parentElement.nextElementSibling.style.display = "none";
+    })
 })
 
 
